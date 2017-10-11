b9f0e9455a81a78a643106b132326012cca3994e
Eleanor Demis <eleanordemis@gmail.com>
2017-10-11 16:00:36 -0700

    configures viper to read app id and app key

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/spf13/viper"
 )
 
 func main() {
 	viper.AutomaticEnv()
-	key := viper.Get("api_key")
-	fmt.Println(key)
+	app_id := viper.Get("edamam_app_id")
+	app_key := viper.Get("edamam_app_key")
+	fmt.Println(app_id)
+	fmt.Println(app_key)
 }
