2860b4390409e8c9446cb03e9009976b01a4ac25
Eleanor Demis <eleanordemis@gmail.com>
2017-10-12 17:34:31 -0700

    flesh out response structs

--- main.go
+++ main.go
@@ -9,8 +9,25 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Response struct {
+	Q    string `json:q`
+	Hits []Hit  `json:hits`
+}
+
+type Hit struct {
+	Recipe Recipe `json:recipe`
+}
+
 type Recipe struct {
-	Q string `json:q`
+	Yield       int          `json:yield`
+	Label       string       `json:label`
+	Calories    float64      `json:calories`
+	Ingredients []Ingredient `json:ingredients`
+}
+
+type Ingredient struct {
+	Text   string `json:text`
+	Weight string `json:weight`
 }
 
 func main() {
@@ -18,11 +35,10 @@ func main() {
 	app_id := viper.Get("edamam_app_id")
 	app_key := viper.Get("edamam_app_key")
 	url := "https://api.edamam.com/search"
-
-	queryString := fmt.Sprintf("%v?q=chicken&app_id=%v&app_key=%v", url, app_id, app_key)
+	queryString := fmt.Sprintf("%v?q=chicken&app_id=%v&app_key=%v&from=0&to=1", url, app_id, app_key)
 	res, _ := http.Get(queryString)
 	body, _ := ioutil.ReadAll(res.Body)
-	var r = new(Recipe)
+	var r = new(Response)
 	json.Unmarshal([]byte(body), &r)
 	fmt.Println(r)
 }
