a02907d1f163e86e4c2e412ff60443c5903d996f
Eleanor Demis <eleanordemis@gmail.com>
2017-10-12 17:55:39 -0700

    parse query param flag

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/spf13/viper"
 )
@@ -36,10 +38,16 @@ func main() {
 	app_id := viper.Get("edamam_app_id")
 	app_key := viper.Get("edamam_app_key")
 	url := "https://api.edamam.com/search"
-	queryString := fmt.Sprintf("%v?q=chicken&app_id=%v&app_key=%v&from=0&to=1", url, app_id, app_key)
+
+	queryParam := flag.String("queryParam", "chicken", "High-level query param you are looking for.")
+
+	flag.Parse()
+
+	queryString := fmt.Sprintf("%v?q=%v&app_id=%v&app_key=%v&from=0&to=1", url, *queryParam, app_id, app_key)
 	res, _ := http.Get(queryString)
 	body, _ := ioutil.ReadAll(res.Body)
 	var r = new(Response)
 	json.Unmarshal([]byte(body), &r)
 	fmt.Println(r)
+	os.Exit(1)
 }
