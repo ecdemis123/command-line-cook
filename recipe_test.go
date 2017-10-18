1ae05c4103a371c4c9d47a8d5382814d0b29bf57
Eleanor Demis <eleanordemis@gmail.com>
2017-10-18 11:43:29 -0700

    updates with custom response

--- recipe_test.go
+++ recipe_test.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+var res = &Response{
+	Q: "chicken",
+}
+
 func TestGetRecipe(t *testing.T) {
+	res2B, _ := json.Marshal(res)
 	var edamamStub = httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(res2B))
 			}))
 
 	res := getRecipe(edamamStub.URL)
